c69fa7953ff3b42f98682254ad36118ef57e7653
arushinofu <makushimu.arushinofu@gmail.com>
2026-03-11 19:58:57 +0300

    playground | Add get_profile function

--- playground/git-branch-practice/src/app.py
+++ playground/git-branch-practice/src/app.py
@@ -5,5 +5,8 @@ def greet():
 if __name__ == "__main__":
     greet()
 
+def get_profile(user_id):
+    print(f"Profile for user {user_id}")
+
 def login(username):
     print(f"User {username} logged in")
